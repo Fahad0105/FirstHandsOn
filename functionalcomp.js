f3a61f090ccbd4efe0333feddaa90dcec55dc44f
Fahad0105 <115901893+Fahad0105@users.noreply.github.com>
2022-10-19 04:22:34 +0530

    Add files via upload

---
+++ functionalcomp.js
@@ -0,0 +1,9 @@
+import React from "react";
+ function functionalcomp(){
+    return( 
+    <div>
+      <p>THIS IS FUNCTIONAL COMPONENTS</p>
+      </div>
+      );
+ }
+ export default functionalcomp;

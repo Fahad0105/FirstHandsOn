1672d40917a5121eec5f9c77e0cf440d040f9b83
Fahad0105 <115901893+Fahad0105@users.noreply.github.com>
2022-10-19 16:17:12 +0530

    Delete functionalcomp.js

--- functionalcomp.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
- function functionalcomp(){
-    return( 
-    <div>
-      <p>THIS IS FUNCTIONAL COMPONENTS</p>
-      </div>
-      );
- }
- export default functionalcomp;
